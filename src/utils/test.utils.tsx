c1622854a8746c610c7438dfc7a5a1f4dabc501d
Matheus Lopes <matlopes1999@gmail.com>
2022-12-31 23:43:23 -0300

    refactor: remove unecesary imports on test-utils file

--- src/utils/test.utils.tsx
+++ src/utils/test.utils.tsx
@@ -2,15 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-import i18next from 'i18next';
-import { I18nextProvider, initReactI18next } from 'react-i18next';
-import Backend from 'i18next-chained-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import { ThemeProvider } from '@/contexts';
 import { store } from '@/redux/store';
-
-const i18n = i18next.use(Backend).use(LanguageDetector).use(initReactI18next);
 
 const ProviderWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const router = createMemoryRouter([
@@ -21,13 +15,11 @@ const ProviderWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) 
     ]);
 
     return (
-        <I18nextProvider i18n={i18n}>
-            <Provider store={store}>
-                <ThemeProvider>
-                    <RouterProvider router={router} />;
-                </ThemeProvider>
-            </Provider>
-        </I18nextProvider>
+        <Provider store={store}>
+            <ThemeProvider>
+                <RouterProvider router={router} />;
+            </ThemeProvider>
+        </Provider>
     );
 };
 
